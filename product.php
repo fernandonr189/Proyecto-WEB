721f3391b4ca90dd1300bc930091e7247d116173
Fernando Navarrete <fernandonr189@outlook.com>
2023-10-26 19:21:27 -0600

    fixed product page

--- product.php
+++ product.php
@@ -1,4 +1,5 @@
 <?php
+    include 'php/connection.php';
     session_start();
     @$user = $_SESSION['name'];
     @$id = $_SESSION['id'];
@@ -20,6 +21,21 @@
             <i class=\"bi-door-open me-1\"> Logout</i>
         </button>
     </form>";
+
+    $productId = $_GET['product_id'];
+    if(!isset($productId)) {
+        header("location: ./index.php");
+    }
+
+    $sql = "SELECT * FROM PRODUCTS WHERE ID =  '$productId'";
+    $result = $conn->query($sql);
+    $conn->close();
+
+    $product = $result->fetch_assoc();
+    $product_name = $product['NAME'];
+    $product_image = $product['IMAGE'];
+    $product_description = $product['DESCRIPTION'];
+    $product_price = $product['PRICE'];
 ?>
 <!DOCTYPE html>
 <html lang="en" class="h-100">
@@ -64,15 +80,15 @@
         <section class="py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
+                    <div class="col-md-6 p-4">
+                        <img class="card-img-top mb-5 mb-md-0" src="php/<?php echo $product_image; ?>" alt="..." />
+                    </div>
                     <div class="col-md-6">
-                        <div class="small mb-1">SKU: BST-498</div>
-                        <h1 class="display-5 fw-bolder">Shop item template</h1>
+                        <h1 class="display-5 fw-bolder"><?php echo $product_name; ?></h1>
                         <div class="fs-5 mb-5">
-                            <span class="text-decoration-line-through">$45.00</span>
-                            <span>$40.00</span>
+                            <span>$<?php echo $product_price; ?></span>
                         </div>
-                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
+                        <p class="lead"><?php echo $product_description; ?></p>
                         <div class="d-flex">
                             <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
                             <button class="btn btn-outline-dark flex-shrink-0" type="button">
--- products.php
+++ products.php
@@ -1,4 +1,5 @@
 <?php
+    include 'php/connection.php';
     session_start();
     @$user = $_SESSION['name'];
     @$id = $_SESSION['id'];
@@ -28,6 +29,10 @@
             <i class=\"bi bi-plus-circle me-1\"> Add product</i>
         </button>
     </form>";
+
+    $sql = "SELECT * FROM PRODUCTS ORDER BY ID DESC";
+    $result = $conn->query($sql);
+    $conn->close();
 ?>
 <!DOCTYPE html>
 <html lang="en" class="h-100">
@@ -80,64 +85,41 @@
         <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name-->
-                                    <a href="product.php" class="link-dark text-decoration-none">
-                                        <h5 class="fw-bolder">Popular Item</h5>
-                                    </a>
-                                    <!-- Product reviews-->
-                                    <div class="d-flex justify-content-center small text-warning mb-2">
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
+                    <?php
+                        while($rows = $result->fetch_assoc()) {
+                            echo "
+                            <div class=\"col mb-5\">
+                                <div class=\"card h-100\">
+                                    <!-- Product image-->
+                                    <img class=\"card-img-top p-5 \" src=\"php/" . $rows['IMAGE'] . "\" alt=\"...\" />
+                                    <!-- Product details-->
+                                    <div class=\"card-body p-4\">
+                                        <div class=\"text-center\">
+                                            <!-- Product name-->
+                                            <a href=\"product.php" . "?product_id=" . $rows['ID'] . " \" class=\"link-dark text-decoration-none\">
+                                                <h5 class=\"fw-bolder\">" . $rows['NAME'] . "</h5>
+                                            </a>
+                                            <!-- Product reviews-->
+                                            <div class=\"d-flex justify-content-center small text-warning mb-2\">
+                                                <div class=\"bi-star-fill\"></div>
+                                                <div class=\"bi-star-fill\"></div>
+                                                <div class=\"bi-star-fill\"></div>
+                                                <div class=\"bi-star-fill\"></div>
+                                                <div class=\"bi-star-fill\"></div>
+                                            </div>
+                                            <!-- Product price-->
+                                            " . $rows['PRICE'] . "
+                                        </div>
                                     </div>
-                                    <!-- Product price-->
-                                    $40.00
+                                    <!-- Product actions-->
+                                    <div class=\"card-footer p-4 pt-0 border-top-0 bg-transparent\">
+                                        <div class=\"text-center\"><a class=\"btn btn-outline-dark mt-auto\" href=\"product.php\">Add to cart</a></div>
+                                    </div>
                                 </div>
                             </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="product.php">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name-->
-                                    <a href="product.php" class="link-dark text-decoration-none">
-                                        <h5 class="fw-bolder">Popular Item</h5>
-                                    </a>
-                                    <!-- Product reviews-->
-                                    <div class="d-flex justify-content-center small text-warning mb-2">
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                    </div>
-                                    <!-- Product price-->
-                                    $40.00
-                                </div>
-                            </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="product.php">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
+                            ";
+                        }
+                    ?>
                 </div>
             </div>
         </section>
